f288d61d4c3f8bef3a9c7bbc3b290edbc044e30f
Oanhnguyen275 <lohoangoanh275@gmail.com>
2024-10-27 14:48:45 -0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "d66275f1092470d1bd9a78c59d5097a1"
+
+# Geoapify API Key
+geoapify_key = "ad9ff1c83e284a749a23e9b0dd6b6639"
+
+# Fred API
+fred_api = "5e99e459e65879aa5e79d6198946d3e6"
---
+++ config.py
@@ -0,0 +1,1 @@
+weather_api_key = "fd51c2ee7d3bba227eb14c71feb5769b"
